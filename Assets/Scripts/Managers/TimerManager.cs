043cb16c9e6c6a36f65a559c87db87a76338d948
mikeleo03 <leonmichael463@gmail.com>
2024-04-29 10:02:18 +0700

    feat : timer manager

---
+++ Assets/Scripts/Managers/TimerManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour {
+
+    public Text Timer;
+    private double time = 0;
+    private bool isRunning = false;
+
+    private void Awake() {
+        StartTimer();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (isRunning) {
+            time = time + Time.deltaTime;
+        }
+        string timeText = System.TimeSpan.FromSeconds(time).ToString("mm':'ss");
+        Timer.text = timeText;
+    }
+
+    public void ResetTimer() {
+        time = 0;
+    }
+
+    public double TakeTime() {
+        StopTimer();
+        var r = time;
+        ResetTimer();
+        return r;
+    }
+
+    public void StartTimer() {
+        isRunning = true;
+    }
+
+    public void StopTimer() {
+        isRunning = false;
+    }
+}
