f1915d5d61ad77504ce25220094fef291b3c2a16
billc27 <110593711+billc27@users.noreply.github.com>
2024-05-07 04:09:41 +0700

    feat: increase damage orb

--- Assets/Scripts/Orbs/IncreaseDamageOrbs.cs
+++ Assets/Scripts/Orbs/IncreaseDamageOrbs.cs
@@ -1,11 +1,31 @@
+using Nightmare;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class IncreaseDamageOrbs : Orbs
 {
-    public override void ApplyOrbEffect(GameObject Player)
+    public PlayerShooting playerShooting;
+
+    public static int damageOrbCount = 0;
+
+    public override void Start()
     {
-        Debug.Log("ID");
+        playerShooting = GameObject.Find("Player").GetComponentInChildren<PlayerShooting>();
+
+        // Destroy object after 5 seconds
+        Destroy(gameObject, 5f);
+    }
+
+    public override void ApplyOrbEffect()
+    {
+        if (damageOrbCount < 15) // Maximum orb effects a player can get
+        {
+            playerShooting.damagePercent += 0.1f; // Add 10% from base damage
+            damageOrbCount += 1;
+        }
+
+        // Destroy the orb
+        Destroy(gameObject);
     }
 }
