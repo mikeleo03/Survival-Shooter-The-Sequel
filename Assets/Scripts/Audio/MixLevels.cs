94b58fba3f23d4926f01e2a7637e72ef4b68fab9
Nat10k <110515021+Nat10k@users.noreply.github.com>
2024-05-07 16:07:19 +0700

    feat : synchronize volume setting at main and pause

--- Assets/Scripts/Audio/MixLevels.cs
+++ Assets/Scripts/Audio/MixLevels.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MixLevels : MonoBehaviour {
 
 	public AudioMixer masterMixer;
+	[SerializeField] private Slider sfxSlider, musicSlider;
 
-	public void SetSfxLvl(float sfxLvl)
+    private void Awake()
+    {
+		sfxSlider.value = PlayerPrefs.GetFloat("sfxVol", -10);
+        musicSlider.value = PlayerPrefs.GetFloat("musicVol", -10);
+    }
+
+    public void SetSfxLvl(float sfxLvl)
 	{
 		masterMixer.SetFloat("sfxVol", sfxLvl);
+		PlayerPrefs.SetFloat("sfxVol", sfxLvl);
 	}
 
 	public void SetMusicLvl (float musicLvl)
 	{
 		masterMixer.SetFloat ("musicVol", musicLvl);
-	}
+        PlayerPrefs.SetFloat("musicVol", musicLvl);
+    }
 }
